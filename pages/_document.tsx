6da3f62ed527d6407bdac0ea87c78a42cbb486fe
Tristan Barrow <tristanmarkbarrow@gmail.com>
2021-08-07 00:22:29 -0600

    added federation consumer

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script
+            data-webpack="wmProvider"
+            src="https://provider-alpha.vercel.app/_next/static/chunks/remoteEntry.js"
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
