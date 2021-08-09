12e25b9f3e3e88d79d7bf275f8d934a8da7d1ceb
Tristan Barrow <tristanmarkbarrow@gmail.com>
2021-08-09 10:02:14 -0600

    tying link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
-// import Script from 'next/script';
+import Script from 'next/script';
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -9,13 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
-        <body>
-          <script
-            async
+        <Head>
+          <link rel='preconnect' href='https://provider-alpha.vercel.app/_next/static/chunks/remoteentry.js' />
+          <Script
             data-webpack="wmProvider"
-            src="https://provider-alpha.vercel.app/_next/static/chunks/remoteEntry.js"
+            src="https://provider-alpha.vercel.app/_next/static/chunks/remoteentry.js"
           />
+        </Head>
+        <body>
           <Main />
           <NextScript />
         </body>
